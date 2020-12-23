6b2de0a48da4c4657984f696c207260071073aa2
idobetesh <idobetesh@gmail.com>
2020-12-23 22:16:30 +0200

    sorted dates

--- EX2 - Tweets Analysis/main.py
+++ EX2 - Tweets Analysis/main.py
@@ -12,21 +12,23 @@ btc = re.compile(r'#bitcoin|#btc')
 def summary(file_path):
     with open(file_path, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        mySet = set()
         for row in reader:
             if btc.search(dict(row)['text'].lower()) is not None:
                 #print("===========================", dict(row)['text'])
                 continue
             if web.search(dict(row)['text']) is not None:
-                #print("******************",dict(row)['text'])
+                # print("******************",dict(row)['text'])
                 continue
-            tmp = dict(row)['timestamp'][:7]
-            print(tmp)
+            # mySet.add(dt.datetime.strptime(dict(row)['timestamp'][:7], '%Y-%m').date())
+            mySet.add(dict(row)['timestamp'][:7])
+        a = list(mySet)
+        a = sorted(a)
+        print(a)
 
-            
 
 
-            
-            
 if __name__ == '__main__':
-    #summary("t.csv")
+    # summary("t.csv")
     summary("tweets_small_data.csv")
+    # summary("tweets.csv")
