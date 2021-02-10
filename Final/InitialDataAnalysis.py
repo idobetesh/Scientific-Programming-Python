3a6843f28f64c8a9b2ae966edf52a8fe0f5727c2
idobetesh <idobetesh@gmail.com>
2021-02-10 23:31:55 +0200

    correct clean_customers.csv added

--- Final/InitialDataAnalysis.py
+++ Final/InitialDataAnalysis.py
@@ -40,16 +40,35 @@ df.Shop_Meat.fillna(df.Shop_Meat.median())
 # Keep all float numbers 3 digits rounded
 df = df.round(3)
 
-# Drop all Categorical 'na'
+# Keep all categorical data in lower case
+df.Spending_Score = df.Spending_Score.str.lower()
+
+# Lastly, drop all Categorical 'na'
 df = df.dropna()
 
-# df = df.dropna(axis=0, subset=['Profession'])
-print(df.info)
-df.to_csv('Data/clean.csv')
+# df.to_csv('Data/clean_customers.csv')
 # df['Profession'].hist(color='orange', bins=len(df['Profession'].unique()), edgecolor='blue')
-# plt.title('Profession Distribution')
+# plt.title('Profession Distribution on Clean Data')
 # plt.ylabel('Count', color='blue')
 # plt.xlabel('Profession', color='blue')
 # plt.tight_layout()
 # plt.grid(True)
 # plt.show()
+
+df['Gender'].hist(bins=len(df['Gender'].unique()), edgecolor='orange')
+plt.title('Gender Distribution on Clean Data')
+plt.ylabel('Count', color='blue')
+plt.xlabel('Gender', color='blue')
+plt.tight_layout()
+plt.grid(True) 
+plt.show()
+
+print(f"Healthcare count: {sum(df['Profession'] == 'Healthcare')}")
+print(f"Engineer count: {sum(df['Profession'] == 'Engineer')}")
+print(f"Lawyer count: {sum(df['Profession'] == 'Lawyer')}")
+print(f"Entertainment count: {sum(df['Profession'] == 'Entertainment')}")
+print(f"Artist count: {sum(df['Profession'] == 'Artist')}")
+print(f"Executive count: {sum(df['Profession'] == 'Executive')}")
+print(f"Doctor count: {sum(df['Profession'] == 'Doctor')}")
+print(f"Homemaker count: {sum(df['Profession'] == 'Homemaker')}")
+print(f"Marketing count: {sum(df['Profession'] == 'Marketing')}")
