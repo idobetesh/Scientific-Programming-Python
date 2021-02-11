73c2c58d43f5cfac8081c931c2620494f5711120
idobetesh <idobetesh@gmail.com>
2021-02-11 13:34:47 +0200

    regression and Avg_Shop col

--- Final/task3_ExploratoryDataAnalysis.py
+++ Final/task3_ExploratoryDataAnalysis.py
@@ -6,18 +6,24 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('./Data/clean_customers.csv')
+df = pd.read_csv('./Data/avg.csv')
 
 # x_vars=['Shop_Other','Shop_Dairy','Shop_Household','Shop_Meat'], y_vars=['Age', 'Work_Experience'], 
 # sns.pairplot(df, hue='Group', hue_order=['A','B','C','D'], height=1)
 # plt.show()
 
+# df['Avg_Shop'] = (df['Shop_Other'] + df['Shop_Dairy'] + df['Shop_Household'] + df['Shop_Meat']) / 4
+# df = df.round(3)
 
-
-df['Shop_Day'].hist(bins=len(df['Shop_Day'].unique()), edgecolor='orange')
-plt.title('Shop_Day Distribution')
-plt.ylabel('Count', color='blue')
-plt.xlabel('Shop_Day', color='blue')
-plt.tight_layout()
-plt.grid(True) 
+# x_vars=['Shop_Other','Shop_Dairy','Shop_Household','Shop_Meat'], y_vars=['Avg_Shop'], 
+sns.pairplot(df, hue='Profession', x_vars=['Shop_Other','Shop_Dairy','Shop_Household','Shop_Meat'], y_vars=['Avg_Shop'], kind='reg')
 plt.show()
+
+
+# df['Shop_Day'].hist(bins=len(df['Shop_Day'].unique()), edgecolor='orange')
+# plt.title('Shop_Day Distribution')
+# plt.ylabel('Count', color='blue')
+# plt.xlabel('Shop_Day', color='blue')
+# plt.tight_layout()
+# plt.grid(True) 
+# plt.show()
