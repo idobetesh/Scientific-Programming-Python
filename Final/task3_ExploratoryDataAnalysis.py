c896edccc08e38e4fb369e5b2860c09c9c39bf71
idobetesh <idobetesh@gmail.com>
2021-02-15 14:32:05 +0200

    numeric columns -> range base

--- Final/task3_ExploratoryDataAnalysis.py
+++ Final/task3_ExploratoryDataAnalysis.py
@@ -6,8 +6,10 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
-# df = pd.read_csv('./Data/clean_customers.csv')
-df = pd.read_csv('./Data/clean_customers2.csv')
+df = pd.read_csv('./Data/clean_customers.csv')
+# df = pd.read_csv('./Data/avg.csv')
+# df = pd.read_csv('./Data/clean_customers2.csv')
+# print(df)
 
 # x_vars=['Shop_Other','Shop_Dairy','Shop_Household','Shop_Meat'], y_vars=['Age', 'Work_Experience'], 
 # sns.pairplot(df, hue='Group', hue_order=['A','B','C','D'], height=1)
@@ -23,43 +25,33 @@ df = pd.read_csv('./Data/clean_customers2.csv')
 # df['Spending_Score'] = pd.Categorical(df.Spending_Score, ordered=True, categories=['high', 'average','low']).codes+1
 # df['Group'] = pd.Categorical(df.Group, ordered=True, categories=['A','B','C','D']).codes+1
 
-# age = pd.cut(df['Age'], [18,25,40,60,100])
+# age = pd.cut(df['Age'], [18,30,40,50,60,100])
 # pd.set_option('display.max_rows', None, 'display.max_columns', None)
-# print(df.pivot_table('Avg_Shop', [age, 'Group', 'Spending_Score']))
+# print(df.pivot_table('Avg_Shop', [age, 'Group', 'Gender']))
 
 
 # df['Animal_Product_Avg'] = (df['Shop_Dairy'] + df['Shop_Meat']) / 2
 # pd.set_option('display.max_rows', None, 'display.max_columns', None)
 # print(df.pivot_table('Shop_Meat', ['Group']))
 # print(df.pivot_table('Shop_Dairy', ['Group']))
-# print(df.pivot_table('Ever_Married', ['Group']))
+# print(df.pivot_table('Animal_Product_Avg', ['Group']))
 
-# COUNT_PRECENT_TABLE = pd.DataFrame()
-# COUNT_PRECENT_TABLE["count"] = df["Group"].value_counts()
-# COUNT_PRECENT_TABLE["count%"] = (df["Group"].value_counts()/df.shape[0])*100
-# print(COUNT_PRECENT_TABLE)
-
-
-# df_oh = pd.concat([df, pd.get_dummies(df['Gender'])], axis=1)
-# df_oh = pd.concat([df_oh, pd.get_dummies(df['Group'])], axis=1)
-# df_oh = pd.concat([df_oh, pd.get_dummies(df['Spending_Score'])], axis=1)
-# df_oh = pd.concat([df_oh, pd.get_dummies(df['Profession'])], axis=1)
-# df_oh = df_oh.drop(['Work_Experience'], axis=1)
-# df_oh = df_oh.drop(['Shop_Day'], axis=1)
-# df_oh = df_oh.drop(['Shop_Other'], axis=1)
-# df_oh = df_oh.drop(['Family_Size'], axis=1)
-
-# df = pd.concat([df, pd.get_dummies(df["Gender"], prefix="Gender")], axis=1)
-# df = pd.concat([df, pd.get_dummies(df["Ever_Married"], prefix="Ever_Married")], axis=1)
-# df = pd.concat([df, pd.get_dummies(df["Graduated"], prefix="Graduated")], axis=1)
-# df = pd.concat([df, pd.get_dummies(df["Profession"], prefix="Profession")], axis=1)
-# df["Age_Range"] = np.where(df.Age>=60, "60+", np.where(df.Age>=50, "50-60", np.where(df.Age>=40, "40-50", np.where(df.Age>=30, "30-40", np.where(df.Age>=18, "18-30", "18-")))))
-# df["Work_Experience_Range"] = np.where(df.Work_Experience>=10, "10+", np.where(df.Work_Experience>=5, "5-10", "0-5"))
-# df["Family_Size_Range"] = np.where(df.Family_Size>=6, "6+", np.where(df.Family_Size>=3, "3-6", "0-3"))
-
-
-
-print(df.groupby(['Profession', 'Gender'])['Spending_Score'].aggregate('mean').unstack())
+print(df.describe())
+df = pd.concat([df, pd.get_dummies(df['Gender'], prefix='Gender')], axis=1)
+df = pd.concat([df, pd.get_dummies(df['Ever_Married'], prefix='Ever_Married')], axis=1)
+df = pd.concat([df, pd.get_dummies(df['Graduated'], prefix='Graduated')], axis=1)
+df = pd.concat([df, pd.get_dummies(df['Profession'], prefix='Profession')], axis=1)
+df['Age_Range'] = np.where(df.Age>=60, '60+', np.where(df.Age>=50, '50-60', np.where(df.Age>=40, '40-50', np.where(df.Age>=30, '30-40', np.where(df.Age>=18, '18-30', '-18')))))
+df['Work_Experience_Range'] = np.where(df.Work_Experience>=10, '10+', np.where(df.Work_Experience>=5, '5-10', '0-5'))
+df['Family_Size_Range'] = np.where(df.Family_Size>=6, '6+', np.where(df.Family_Size>=3, '3-6', '0-3'))
+df['Shop_Other_Range'] = np.where(df.Shop_Other>=20, '20+', np.where(df.Shop_Other>=10, '10-20', np.where(df.Shop_Other>=0, '0-10', '-0'))) # -10,0,10,20
+df['Shop_Dairy_Range'] = np.where(df.Shop_Dairy>=20, '20+', np.where(df.Shop_Dairy>=10, '10-20', np.where(df.Shop_Dairy>=0, '0-10', '-0'))) # -10,0,10,20
+df['Shop_Household_Range'] = np.where(df.Shop_Household>=20, '20+', np.where(df.Shop_Household>=10, '10-20', np.where(df.Shop_Household>=0, '0-10', '-0'))) # -10,0,10,20
+df['Shop_Meat_Range'] = np.where(df.Shop_Meat>=20, '20+', np.where(df.Shop_Meat>=10, '10-20', np.where(df.Shop_Meat>=0, '0-10', '-0'))) # -10,0,10,20
+df['Spending_Score_Level'] = pd.Categorical(df['Spending_Score'], ordered=True, categories=['low', 'average', 'high']).codes+1 
+df['Group_Level'] = pd.Categorical(df['Group'], ordered=True, categories=['A','B','C','D']).codes+1 
+df = df.round(3)
+df.to_csv('Data/clean_customers2.csv', index=False)
 
 
 # df['Shop_Day'].hist(bins=len(df['Shop_Day'].unique()), edgecolor='orange')
@@ -71,25 +63,31 @@ print(df.groupby(['Profession', 'Gender'])['Spending_Score'].aggregate('mean').u
 # plt.show()
 
 
-# sns.violinplot(data=df["Age"], color='yellow')
+# sns.violinplot(data=df['Age'], color='yellow')
 # plt.show()
 # sns.displot(data=df['Work_Experience'], kde=True)
 # plt.show()
 # sns.displot(data=df['Family_Size'], kde=True, color='green')
 # plt.show()
 
-# fig, axarr = plt.subplots(3, 3, figsize=(30, 18))
-# sns.countplot(x="Gender", hue = "Group", data = df, ax=axarr[0][0])
-# sns.countplot(x="Ever_Married", hue = "Group", data = df, ax=axarr[0][1])
-# sns.countplot(x="Graduated", hue = "Group", data = df, ax=axarr[0][2])
-# sns.countplot(x="Profession", hue = "Group", data = df, ax=axarr[1][0])
-# sns.countplot(x="Spending_Score", hue = "Group", data = df, ax=axarr[1][1])
-# sns.countplot(x="Age_Range", hue = "Group", data = df, ax=axarr[2][0])
-# sns.countplot(x="Work_Experience_Range", hue = "Group", data = df, ax=axarr[2][1])
+# fig, ax = plt.subplots(3, 3, figsize=(30, 18))
+# group_order = ['A','B','C','D']
+# sns.countplot(x='Gender', hue='Group', data=df, ax=ax[0][0], hue_order=group_order, palette='husl')
+# sns.countplot(x='Age_Range', hue='Group', data=df, ax=ax[0][1], hue_order=group_order, palette='husl')
+# sns.countplot(x='Profession', hue='Group', data=df, ax=ax[0][2], hue_order=group_order, palette='husl')
+# sns.countplot(x='Spending_Score', hue='Group', data=df, ax=ax[1][0], hue_order=group_order, palette='husl')
+# sns.countplot(x='Ever_Married', hue='Group', data=df, ax=ax[1][1], hue_order=group_order, palette='husl')
+# sns.countplot(x='Graduated', hue='Group', data=df, ax=ax[1][2], hue_order=group_order, palette='husl')
+# sns.countplot(x='Work_Experience_Range', hue='Group', data=df, ax=ax[2][0], hue_order=group_order, palette='husl')
 # plt.show()
 
 ####################### CORR ####################### 
-# corr = df_oh.corr()
+# corr = df.corr()
+# print(corr)
+# fig, ax = plt.subplots(1, figsize=(25,25))
+# sns.heatmap(corr, annot=True, fmt=',.2f')
+# plt.title('Correlation Map', fontsize=20)
+
 # mask = np.triu(np.ones_like(corr, dtype=bool))
 # f, ax = plt.subplots(figsize=(8,6))
 # cmap = sns.diverging_palette(200,10,as_cmap=True)
